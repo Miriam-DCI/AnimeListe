1fa977a57b7cd0079d17b9939fe4ec3c9fbed64c
Miriam <miriam.bonninghoff@dci-student.org>
2023-11-29 13:44:45 +0100

    wechsel zur neuen api jetzt jikan und code neu geschrieben

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-/**
- * @description Funktion zum Hinzufügen von Anime oder Manga zur Watchlist
- * @param {string} type - "A" für Anime, "M" für Manga
- */
-// Hauptfunktion für das Hinzufügen von Anime oder Manga zur Watchlist
-async function addToWatchedList(type) {
-  const inputElement = document.getElementById("animeInput");
-  const title = inputElement.value.trim();
-
-  if (title !== "") {
-    addToLocalStorage(type, title);
-
-    inputElement.value = "";
-
-    updateWatchedList();
-  }
-}
-
-// Funktion zum Suchen nach Anime über die Jikan API
 async function searchAnime() {
   const inputElement = document.getElementById("animeInput");
   const query = inputElement.value.trim();
@@ -24,16 +5,11 @@ async function searchAnime() {
   if (query !== "") {
     try {
       const response = await fetch(
-        `https://api.jikan.moe/v4/anime?q=${query}&limit=1`
+        `https://api.jikan.moe/v4/anime?q=${query}&limit=6`
       );
       const data = await response.json();
 
-      if (data.results.length > 0) {
-        const anime = data.results[0];
-        displayAnimeInfo(anime);
-      } else {
-        alert("No anime found!");
-      }
+      displayAnimeCards(data.results);
     } catch (error) {
       console.error("Error fetching anime data:", error);
       alert("Error fetching anime data. Please try again later.");
@@ -41,69 +17,20 @@ async function searchAnime() {
   }
 }
 
-// Funktion zum Anzeigen von Anime-Informationen auf der Seite
-function displayAnimeInfo(anime) {
-  const animeElement = document.getElementById("anime");
-  animeElement.innerHTML = `
-    <h2>${anime.title}</h2>
-    <img src="${anime.image_url}" alt="${anime.title}">
-    <p>${anime.synopsis}</p>
-    <button onclick="addToWatchedList('A')">Add to Watched List</button>
-  `;
-}
+function displayAnimeCards(animeList) {
+  const cardContainer = document.getElementById("cardContainer");
+  cardContainer.innerHTML = "";
 
-// Funktion zum Hinzufügen von Anime oder Manga zur Watchlist in localStorage
-function addToLocalStorage(type, title) {
-  let watchedList = JSON.parse(localStorage.getItem("watchedList")) || [];
-  watchedList.push({ type, title });
-  localStorage.setItem("watchedList", JSON.stringify(watchedList));
-}
-
-// Funktion zum Abrufen der Watchlist aus dem localStorage
-function getWatchedListFromLocalStorage() {
-  return JSON.parse(localStorage.getItem("watchedList")) || [];
-}
-
-// Funktion zum Aktualisieren der Watchlist auf der Seite
-function updateWatchedList() {
-  const watchedListElement = document.getElementById("watchedList");
-  watchedListElement.innerHTML = "";
-
-  const watchedList = getWatchedListFromLocalStorage();
-
-  watchedList.forEach((entry, index) => {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `
-      <span>${entry.type === "A" ? "Anime" : "Manga"}: ${entry.title}</span>
-      <button onclick="editEntry(${index})">Edit</button>
-      <button onclick="deleteEntry(${index})">Delete</button>
+  animeList.forEach((anime) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+      <img src="${anime.image_url}" alt="${anime.title}">
+      <h3>${anime.title}</h3>
+      <p>${anime.synopsis}</p>
+      <p>Rating: ${anime.score}</p>
+      <button onclick="addToWatchedList('A', '${anime.title}')">Add to Watched List</button>
     `;
-    watchedListElement.appendChild(listItem);
+    cardContainer.appendChild(card);
   });
-}
-
-// Funktion zum Löschen eines Eintrags aus der Watchlist
-function deleteEntry(index) {
-  const watchedList = getWatchedListFromLocalStorage();
-  const isConfirmed = confirm(
-    `Are you sure you want to delete "${watchedList[index].title}"?`
-  );
-
-  if (isConfirmed) {
-    watchedList.splice(index, 1);
-    localStorage.setItem("watchedList", JSON.stringify(watchedList));
-    updateWatchedList();
-  }
-}
-
-// Funktion zum Bearbeiten eines Eintrags in der Watchlist
-function editEntry(index) {
-  const watchedList = getWatchedListFromLocalStorage();
-  const updatedTitle = prompt("Edit entry:", watchedList[index].title);
-
-  if (updatedTitle !== null) {
-    watchedList[index].title = updatedTitle;
-    localStorage.setItem("watchedList", JSON.stringify(watchedList));
-    updateWatchedList();
-  }
 }
