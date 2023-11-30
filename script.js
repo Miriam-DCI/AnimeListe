9493623a836fd98304747dc7cc9e3d3e3aa38fa4
Miriam <miriam.bonninghoff@dci-student.org>
2023-11-30 10:55:05 +0100

    schönheitsfehler behoben

--- script.js
+++ script.js
@@ -1,55 +1,74 @@
 // Initialisiere die Liste mit Daten aus dem LocalStorage
-let animeList = JSON.parse(localStorage.getItem("animeList")) || [];
+const animeList = JSON.parse(localStorage.getItem("animeList")) || [];
 
 // Funktion zum Anzeigen der Liste
-function displayList() {
+function displayAnimeList() {
   const listContainer = document.getElementById("animeList");
   listContainer.innerHTML = "";
 
   animeList.forEach((item, index) => {
     const listItem = document.createElement("div");
+    listItem.id = `animeItem_${index}`; // Füge eine eindeutige ID hinzu, falls benötigt
     listItem.classList.add("anime-item");
-    listItem.innerHTML = `
-      <span>${item}</span>
-      <button class="btn btn-edit" onclick="editItem(${index})">Bearbeiten</button>
-      <button class="btn btn-delete" onclick="deleteItem(${index})">Löschen</button>
-    `;
+
+    const spanElement = document.createElement("span");
+    spanElement.textContent = item;
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button");
+
+    const editButton = document.createElement("button");
+    editButton.classList.add("btn", "btn-edit");
+    editButton.textContent = "Bearbeiten";
+    editButton.onclick = () => editAnimeItem(index);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("btn", "btn-delete");
+    deleteButton.textContent = "Löschen";
+    deleteButton.onclick = () => deleteAnimeItem(index);
+
+    buttonContainer.appendChild(editButton);
+    buttonContainer.appendChild(deleteButton);
+
+    listItem.appendChild(spanElement);
+    listItem.appendChild(buttonContainer);
+
     listContainer.appendChild(listItem);
   });
 }
 
 // Funktion zum Hinzufügen von Einträgen
-function addToList() {
+function addToAnimeList() {
   const inputElement = document.getElementById("animeInput");
-  const newItem = inputElement.value.trim();
+  const newAnime = inputElement.value.trim();
 
-  if (newItem !== "") {
-    animeList.push(newItem);
-    inputElement.value = "";
+  if (newAnime !== "") {
+    animeList.push(newAnime);
     updateLocalStorage();
-    displayList();
+    displayAnimeList();
+    inputElement.value = ""; // Eingabefeld leeren nach dem Hinzufügen
   }
 }
 
 // Funktion zum Bearbeiten von Einträgen
-function editItem(index) {
+function editAnimeItem(index) {
   const updatedItem = prompt("Bearbeite den Eintrag:", animeList[index]);
   if (updatedItem !== null) {
     animeList[index] = updatedItem.trim();
     updateLocalStorage();
-    displayList();
+    displayAnimeList();
   }
 }
 
 // Funktion zum Löschen von Einträgen
-function deleteItem(index) {
+function deleteAnimeItem(index) {
   const confirmDelete = confirm(
     "Bist du sicher, dass du diesen Eintrag löschen möchtest?"
   );
   if (confirmDelete) {
     animeList.splice(index, 1);
     updateLocalStorage();
-    displayList();
+    displayAnimeList();
   }
 }
 
@@ -59,16 +78,20 @@ function updateLocalStorage() {
 }
 
 // API Jikan zum Abrufen von Informationen zu Anime
-
 function searchAnime() {
   const animeName = document.getElementById("animeInput").value;
-  const animeURL = `https://api.jikan.moe/v3/search/anime?q=${animeName}`;
+  const animeURL = `https://api.jikan.moe/v4/search/anime?q=${animeName}`;
 
   fetch(animeURL)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Fehlerhafte API-Antwort");
+      }
+      return response.json();
+    })
     .then((data) => {
-      const animeList = document.getElementById("animeList");
-      animeList.innerHTML = "";
+      const animeListContainer = document.getElementById("animeList");
+      animeListContainer.innerHTML = "";
 
       data.results.forEach((anime) => {
         const animeItem = document.createElement("div");
@@ -78,10 +101,13 @@ function searchAnime() {
           <span>${anime.title}</span>
           <button class="btn btn-add" onclick="addToAnimeList('${anime.title}')">Hinzufügen</button>
         `;
-        animeList.appendChild(animeItem);
+        animeListContainer.appendChild(animeItem);
       });
+    })
+    .catch((error) => {
+      console.error("Fehler bei der API-Anfrage:", error.message);
     });
 }
 
 // Initialanzeige der Liste
-displayList();
+displayAnimeList();
